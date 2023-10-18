344f8ec6a03a900ebfe3d7a3d676ef3c6cf190b4
biru60 <u1904060@student.cuet.ac.bd>
2023-10-19 00:12:27 +0600

    sign in and footer added

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -3,8 +3,9 @@ import { Link } from "react-router-dom";
 import { AuthContext } from "../AuthProvider/AuthProvider";
 
 
+
 const Register = () => {
-    const{user,createUser,profileUpdate}=useContext(AuthContext);
+    const{createUser,profileUpdate}=useContext(AuthContext);
     const [success,setSuccess]=useState('');
     const [registerError,setRegisterError]=useState('')
 const handleRegister=(e)=>{
@@ -14,15 +15,16 @@ const handleRegister=(e)=>{
  const photo=form.photo.value;
  const email=form.email.value;
  const password=form.password.value;
- setRegisterError('')
- setSuccess('')
+ setRegisterError('');
+ setSuccess('');
  if(password.length<6){
      setRegisterError('password Should be 6 or more');
      e.target.reset();
     return;
  }
- if(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$/.test(password)){
-    setRegisterError("Password should Contain minimum six characters, at least one letter, one number and one special character ")
+ if(!/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/.test(password)){
+    setRegisterError("Password should Contain minimum six characters, at least one uppercase and one lower letter, one number and one special character ")
+
     e.target.reset();
     return;
  }
@@ -30,20 +32,26 @@ const handleRegister=(e)=>{
  .then(result=>{
     console.log(result.user);
     setSuccess('User Created Successfully')
+    e.target.reset()
+      
     profileUpdate(name,photo)
-    .then(
+    .then({
+       
+    }
     )
     .then(error=>
         {
-            console.log(error.message);
+           console.log(error.message);
         })
-        e.target.reset();
  })
- .then(error=>{
+.catch(error=>{
+    e.target.reset()
     console.log(error.message);
-    e.target.reset();
+    setRegisterError(error.message);
+   
  })
- 
+
+
 }
     return (
         <div  className=" bg-login   bg-cover flex flex-col justify-center h-[100vh] items-center p-2 ">
@@ -51,14 +59,20 @@ const handleRegister=(e)=>{
         <form className="max-w-screen-md w-full my-10" onSubmit={handleRegister}>
         <div className=" flex  flex-col gap-4">
          <p className="text-3xl">Enter User Name</p>
-         <input className="w-full h-12 bg-transparent border-4 border-sky-400 text-lg rounded-md" type="text" name="name"  />
+         <input className="w-full h-12 bg-transparent border-4 border-sky-400 text-lg rounded-md" type="text" name="name" required />
          <p className="text-3xl">Enter Profile Image Url</p>
-         <input className="w-full h-12 bg-transparent border-4 border-sky-400 text-lg rounded-md" type="text" name="photo"  />
+         <input className="w-full h-12 bg-transparent border-4 border-sky-400 text-lg rounded-md" type="text" name="photo" required />
          <p className="text-3xl">Enter  Email</p>
-         <input className="w-full h-12 bg-transparent border-4 border-sky-400 text-lg rounded-md" type="email" name="email"  />
+         <input className="w-full h-12 bg-transparent border-4 border-sky-400 text-lg rounded-md" type="email" name="email" required />
          <p className="text-3xl">Enter  Password</p>
-          <input className=" w-full h-12 bg-transparent border-4 border-sky-400 text-xl rounded-md" type="password" name="password"  />
+          <input className=" w-full h-12 bg-transparent border-4 border-sky-400 text-xl rounded-md" type="password" name="password" required  />
           <input className="btn border-4 btn-outline text-sky-500 w-full  font-semibold text-2xl text white  " type="submit" value="Register" />
+          {
+            success?<p className="text-sm text-green-600">{success}</p>:""
+          }
+          {
+            registerError?<p className="text-sm text-red-600">{registerError}</p>:''
+          }
           <div className="flex justify-between items-center">
           <p className="text-xl font-medium">Already Have An Account?</p>
           <Link to="/signin"><button className="btn btn-outline border-4 text-sky-400">Log In</button></Link>
--- src/Pages/SignIn/SignIn.jsx
+++ src/Pages/SignIn/SignIn.jsx
@@ -1,24 +1,64 @@
 import { Link } from "react-router-dom";
+import { AuthContext } from "../AuthProvider/AuthProvider";
+import { useContext, useState } from "react";
 
 
 const SignIn = () => {
+    const {logInUser,googleLogIn}=useContext(AuthContext);
+    const [success,setSuccess]=useState('')
+    const [signInError,setSignInError]=useState('')
+    const handleSignIn=e=>{
+        e.preventDefault();
+        const form=e.target;
+        const email=form.email.value;
+        const password=form.password.value;
+        form.reset()
+        setSuccess('')
+        setSignInError('')
+        logInUser(email,password)
+        .then(result=>{
+            console.log(result.user);
+            setSuccess('User Log In Successfull');
+        })
+        .catch(error=>{
+            console.log(error.message);
+            setSignInError(error.message);
+        })
+
+    }
+    const handleGoogleLogIn=()=>{
+     googleLogIn()
+     .then(result=>{
+        console.log(result.user);
+        setSuccess('User Log In Successfull');
+     })
+     .catch(error=>{
+        console.log(error.message);
+        setSignInError(error.message);
+     })
+    }
     return (
         <div  className=" bg-login   bg-cover flex flex-col justify-center h-[70vh] items-center p-2 ">
           <h2 className="text-5xl font-bold ">Please Log In</h2>
-          <form className="max-w-screen-md w-full my-10">
+          <form className="max-w-screen-md w-full my-10" onSubmit={handleSignIn}>
           <div className=" flex  flex-col gap-4">
            <p className="text-3xl">Enter Your Email</p>
-           <input className="w-full h-12 bg-transparent border-4 border-sky-400 text-lg rounded-md" type="email" name="email"  />
+           <input className="w-full h-12 bg-transparent border-4 border-sky-400 text-lg rounded-md" type="email" name="email" required />
            <p className="text-3xl">Enter Your Password</p>
-            <input className=" w-full h-12 bg-transparent border-4 border-sky-400 text-xl rounded-md" type="password" name="password"  />
+            <input className=" w-full h-12 bg-transparent border-4 border-sky-400 text-xl rounded-md" type="password" name="password" required  />
             <input className="btn border-4 btn-outline text-sky-500 w-full  font-semibold text-2xl text white  " type="submit" value="Log In" />
+            <button className="btn border-4 btn-outline text-sky-500 w-full  font-semibold text-2xl text white  " onClick={handleGoogleLogIn}>Log In With Google</button>
+            {
+                success?<p className="text-green-400">{success}</p>:''
+            }
+            {
+                signInError?<p className="text-red-500">{signInError}</p>:''
+            }
             <div className="flex justify-between items-center">
             <p className="text-xl font-medium">Do Not Have An Account?</p>
             <Link to="/register"><button className="btn btn-outline border-4 text-sky-400">Register Now</button></Link>
             </div>
           </div>
-
-
 
           </form>
         </div>
